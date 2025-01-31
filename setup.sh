06baeda74628142bd7a02a1188505c050ace56f4
herm1t <72761718+herm1t0@users.noreply.github.com>
2025-01-31 16:01:47 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,49 +7,83 @@
 #################################################
 # Packages to install.
 #################################################
-CLI_MAIN_PACKAGES="git linux-headers polkit"
+
+SHELL="zsh"
+WAYLAND_COMPOSITOR="hyprland"
 CLI_TEXT_EDITOR="micro"
-FONT_PACKAGES="ttf-noto-nerd ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols"
+
+FONTS="ttf-noto-nerd ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols"
 GPU_DRIVER_PACKAGES="nvidia-open-dkms nvidia-utils"
+TERMINAL_EMULATOR="alacritty"
+BROWSER="firefox"
+APP_LAUNCHER="fuzzel"
+FILE_MANAGER="nemo"
+SOUND_MIXER="pavucontrol"
+MISC_PACKAGES="dconf-editor font-manager git linux-headers polkit"
 
-# Hyprland packages (GUI only) alacritty
-
-HYPR_MAIN_PACKAGES="hyprland"
-HYPR_CONSOLE_EMULATOR="alacritty"
-HYPR_BROWSER="firefox"
-HYPR_APP_LAUNCHER="fuzzel"
-HYPR_FILE_MANAGER="nemo"
-HYPR_SOUND_MIXER="pavucontrol"
-HYPR_MISC_PACKAGES="dconf-editor font-manager"
+#AUR_PACKAGES
+GUI_TEXT_EDITOR="visual-studio-code-bin"
+AUR_PACKAGES="uwsm"
 
 
-PACKS_TO_INSTALL=(
+PACMAN_PACKAGES=(
     
 
 )
 
+AUR_PACKAGES=(
 
 
+)
 
-for pack in ${PACKS_TO_INSTALL[@]}; do
-    pacman -Suy --noconfirm --needed --quiet $PACKS_TO_INSTALL
+CWD="${PWD}" # Save cloned directory
+
+for pack in ${PACMAN_PACKAGES[@]}; do
+    pacman -Suy --noconfirm --needed --quiet $pack
 done
 
-read -p "Install AUR helper? (yay) (Y/N): " USER_INPUT
+# Install yay
+read -p "Install AUR helper? (yay) (Y/N): " user_input
 
-if [[ $USER_INPUT == "y" || $USER_INPUT == "Y" ]] 
+if [[ $user_input == "y" || $user_input == "Y" ]] 
 then
-cd ~
-git clone https://aur.archlinux.org/yay.git
-cd yay
--u $USER makepkg -sir
-cd ..
-rm -rf yay/
+    cd ~
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    -u $SUDO_USER makepkg -sir
+    cd ..
+    rm -rf yay/
+    for pack in ${AUR_PACKAGES[@]}; do
+        -u $SUDO_USER yay -S --noconfirm --needed --quiet $pack
+    done
+else
+    echo "yay and AUR packages won't be installed"
 fi
 
-read -p "Install Outline CLI? (Y/N): " USER_INPUT
+# Enable tty autologin
+read -p "Enable tty autologin for the current user? (Y/N): " user_input
 
-if [[ $USER_INPUT == "y" || $USER_INPUT == "Y" ]] 
+if [[ $user_input == "y" || $user_input == "Y" ]] 
+then
+cd $CWD # Back to the main directory
+getty_path="etc/systemd/system/getty@tty1.service.d"
+cp $getty_path/override.conf /$getty_path/
+sed -i "s/username/$SUDO_USER/" /$getty_path/override.conf
+echo "Autologin is enabled"
+fi
+
+# Enable hyprland autostart
+read -p "Launch hyprland on startup? (Y/N): " user_input
+
+if [[ $user_input == "y" || $user_input == "Y" ]] 
+then
+cp .zprofile ~/
+fi
+
+# Install outline VPN
+read -p "Install Outline CLI? (Y/N): " user_input
+
+if [[ $user_input == "y" || $user_input == "Y" ]] 
 then
 cd ~
 git clone https://github.com/Kir-Antipov/outline-cli

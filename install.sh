517100458b387883368dc012dedd4162997b20c7
herm1t <72761718+herm1t0@users.noreply.github.com>
2025-03-12 15:19:52 +0300

    Update and rename setup.sh to install.sh

--- install.sh
+++ install.sh
@@ -1,39 +1,20 @@
 #!/bin/sh
 
-# Linux post-installation setup
+#################################################
+# Linux post installation configuration
+#################################################
 
 repo_url="https://github.com/herm1t0/arch-hyprland-setup"
+pacman_packages_url="$repo_url/blob/main/pacman-packages?raw=true"
+aur_packages_url="$repo_url/blob/main/aur-packages?raw=true"
+functions_url="$repo_url/blob/main/functions?raw=true"
 
-
-
-#################################################
-# Packages to install.
-#################################################
-
-SHELL="zsh"
-WAYLAND_COMPOSITOR="hyprland"
-CLI_TEXT_EDITOR="micro"
-FONTS="ttf-noto-nerd ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols"
-GPU_DRIVER_PACKAGES="nvidia-open-dkms nvidia-utils"
-TERMINAL_EMULATOR="alacritty"
-BROWSER="firefox"
-APP_LAUNCHER="fuzzel"
-FILE_MANAGER="nemo"
-MISC_PACKAGES="dconf-editor font-manager git linux-headers polkit uwsm file-roller pavucontrol xdg-desktop-portal-hyprlandxdg-desktop-portal-gtk hyprpolkitagent qt5-wayland qt6-wayland hyprland-qt-support"
-
-#AUR_PACKAGES
-GUI_TEXT_EDITOR="visual-studio-code-bin"
-AUR_MISC_PACKAGES=""
-
-
-PACMAN_PACKAGES=(
-    $SHELL $WAYLAND_COMPOSITOR $CLI_TEXT_EDITOR $FONTS $GPU_DRIVER_PACKAGES $TERMINAL_EMULATOR
-	$BROWSER $APP_LAUNCHER $FILE_MANAGER $MISC_PACKAGES
-)
-
-AUR_PACKAGES=(
-    $GUI_TEXT_EDITOR $AUR_MISC_PACKAGES
-)
+# Load pacman packages list
+source <(curl -Ls $pacman_packages_url)
+# Load AUR packages list
+source <(curl -Ls $aur_packages_url)
+# Load functions
+source <(curl -Ls $functions_url)
 
 install_pacman_packages()
 {

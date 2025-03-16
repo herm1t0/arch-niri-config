1564c61fa3b8dd9766f0d313413c515c76af8cbd
herm1t0 <gg.smoked@gmail.com>
2025-03-16 18:30:16 +0300

    modified:   README.md 	new file:   config/alacritty/alacritty.toml 	modified:   config/niri/config.kdl 	modified:   functions 	modified:   install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,6 @@
 # Linux post installation configuration
 #################################################
 
-if (( $EUID == 0 )); then
-	echo -e "Please DON'T run as root/sudo\nExitting..."
-	exit
-fi
-
 REPO_URL="https://github.com/herm1t0/arch-niri-config"
 
 SOURCES_LIST=(
@@ -22,16 +17,39 @@ declare -A CONFIG_LIST=(
 	["$REPO_URL/blob/main/config/.zprofile?raw=true"]="$HOME/.zprofile"
 	["$REPO_URL/blob/main/config/.zshrc?raw=true"]="$HOME/.zshrc"
 	["$REPO_URL/blob/main/config/niri/config.kdl?raw=true"]="$HOME/.config/niri/config.kdl"
+	["$REPO_URL/blob/main/config/alacritty/alacritty.toml?raw=true"]="$HOME/.config/alacritty/alacritty.toml"
 	["$REPO_URL/blob/main/config/fuzzel/fuzzel.ini?raw=true"]="$HOME/.config/fuzzel/fuzzel.ini"
 	["$REPO_URL/blob/main/config/fuzzel/theme.ini?raw=true"]="$HOME/.config/fuzzel/theme.ini"
 )
 
-# Include all sources
-for source in ${SOURCES_LIST[@]}; do
-	source <(curl -Ls $source)
-done
 
-#install_packages
-install_configs
+# Main entry point of the script
+main()
+{
+	# Include all sources
+	for source in ${SOURCES_LIST[@]}; do
+		source <(curl -Ls $source)
+	done
 
-echo "Jobs done"
+	# Check is script running as root
+	assert_is_root
+
+	case ${1} in
+		-i|--install)
+			install_configs;;
+		-u|--update)
+			echo "TO DO"; show_help; exit;;
+		-h|--help)
+			show_help; exit;;
+		*)
+			echo "incorrect input ${@}"; show_help; exit;;
+	esac
+
+	#install_packages
+	echo "Jobs done"	
+}
+
+main "${@}"
+
+
+

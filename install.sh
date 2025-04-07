d0c3f087c03a870f42724f545b06a0bac6d40e20
herm1t0 <gg.smoked@gmail.com>
2025-04-07 17:48:26 +0300

    Refactoring

--- install.sh
+++ install.sh
@@ -5,16 +5,15 @@
 #################################################
 
 REPO_URL="https://github.com/herm1t0/arch-niri-config/blob/main"
+JETBRA_PATH="$HOME/jetbra"
 
-export SCRIPT_NAME="${0}"
-
-SOURCES_LIST=(
+declare -a SOURCES_LIST=(
 	"$REPO_URL/pacman-packages?raw=true"
 	"$REPO_URL/aur-packages?raw=true"
 	"$REPO_URL/functions?raw=true"
 )
 
-# Key - url to config, value - config destination
+# Key - config file URL, value - config file destination path
 declare -Ax CONFIG_LIST=(
 	["$REPO_URL/config/.zshenv?raw=true"]="$HOME/.zshenv"
 	["$REPO_URL/config/zsh/.zprofile?raw=true"]="$HOME/.config/zsh/.zprofile"
@@ -22,12 +21,14 @@ declare -Ax CONFIG_LIST=(
 	["$REPO_URL/config/niri/config.kdl?raw=true"]="$HOME/.config/niri/config.kdl"
 	["$REPO_URL/config/fuzzel/fuzzel.ini?raw=true"]="$HOME/.config/fuzzel/fuzzel.ini"
 	["$REPO_URL/config/fuzzel/theme.ini?raw=true"]="$HOME/.config/fuzzel/theme.ini"
+	["$REPO_URL/jetbra/block_url_keywords?raw=true"]="$JETBRA_PATH/block_url_keywords"
+	["$REPO_URL/jetbra/fuzzel/theme.ini?raw=true"]="$JETBRA_PATH/block_url_keywords"
+	["$REPO_URL/jetbra/fuzzel/theme.ini?raw=true"]="$JETBRA_PATH/block_url_keywords"
 )
 
 # Main entry point of the script
 main()
 {
-	
 
 	# Include all sources
 	for src in "${SOURCES_LIST[@]}"; do
@@ -35,25 +36,24 @@ main()
 	done
 
 	# Check is script running as root
-	assert_is_root
+	assert_is_sudo
 
-	echo -e "Choose an option:\n1 - install\n2 - update\n3 - show info\n"
+	printf "%s\n" "Choose an option:" "1 - install" "2 - update" "3 - show info"
 	read -r input
 
 	case "${input}" in
 		1|install)
 			install_packages; install_configs;;
 		2|update)
-			echo -e "NOT IMPLEMENTED YET\n"; exit;;
+			printf "%s\n" "NOT IMPLEMENTED YET"; exit;;
 		3|"show info"|info)
 			show_info; exit;;
 		*)
-			echo -e "Incorrect input\n"; exit;;
+			printf "%s\n" "Incorrect input"; exit;;
 	esac
 	
-	echo "Jobs done"
-
-	# Add reboot with delay
+	printf "%s\n" "Jobs done, don't forget to reboot"
 }
 
+# Calls a main function
 main
